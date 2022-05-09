2e63dc441946d996d9a1fb2dbc77341d15104124
Samantha <samanthax0326@gmail.com>
2022-05-09 15:14:25 -0700

    very basic JS

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,4 @@
+var A = "This is my first string variable and it\'s pretty cool!", B = " This is my second string variable." //These are my variables
+document.write("I made two variables! " + A + " " + B) //These are my concatenated variables
+var expression = "This expression will be an alert!" //This is my expression
+window.alert(expression) //This is my expression as an alert
