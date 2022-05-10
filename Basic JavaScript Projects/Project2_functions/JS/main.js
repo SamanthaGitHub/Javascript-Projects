7bb95e33038e480293997e6e2a79b413c6b7c45e
Samantha <samanthax0326@gmail.com>
2022-05-10 11:55:01 -0700

    project 2 functions

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -9,3 +9,10 @@ function Concatenate_Function() { //this will display the two str variables toge
     str += "and this is the second part";
     document.getElementById("Concatenate").innerHTML = str;
 }
+
+function S2_Function () {
+    var sentence = "This is my own function ";
+    sentence += "and this my own sentence with concatenation";
+    document.getElementById("mySentence").innerHTML = sentence;
+    document.getElementById("mySentence").style.color = "red";
+}
