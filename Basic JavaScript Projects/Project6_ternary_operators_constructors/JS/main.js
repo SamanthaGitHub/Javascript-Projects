fab01ed71a2393c66ec4a1ed172e363e5c1a7f40
samantha <samanthax0326@gmail.com>
2022-05-11 11:37:57 -0700

    initial project 6 ternary

---
+++ Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -0,0 +1,47 @@
+// var var = "slam"
+// document.write(var)
+
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+    document.getElementById("Keywords_and_Constructors").innerHTML =
+    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +
+    " manufactured in " + Erik.Vehicle_Year;
+}
+
+function cat(Name, Age, Color, Toy) {
+    this.cat_Name = Name;
+    this.cat_Age = Age;
+    this.cat_Color = Color;
+    this.cat_Toy = Toy;
+}
+
+var Sprinkles = new cat("Sprinkles", "17", "white", "mouse");
+var Spammy = new cat("Spammy", "1", "black", "cat nip");
+
+function countFunction() {
+    document.getElementById("Counting").innerHTML = Count();
+    function Count() {
+        var startingPoint = 9;
+        function plusOne() {startingPoint += 1;}
+        plusOne();
+        return startingPoint;
+    }
+}
+
+function samFunction() {
+    document.getElementById("Nested_Function").innerHTML = increment()
+    function increment() {
+        var num = 1;
+        num++;
+        return num;
+    }
+}
