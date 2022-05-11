f50037169cf6bf83a8487f0fc70e6c0e0c921bfe
Samantha <samanthax0326@gmail.com>
2022-05-11 11:46:56 -0700

    project 6 ternary

--- Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -1,6 +1,20 @@
 // var var = "slam"
 // document.write(var)
 
+function Ride_Function() {
+    var Height, Can_ride;
+    Height = document.getElementById("Height").value;
+    Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
+}
+
+function voting() {
+    var age, canVote
+    age = document.getElementById("age").value;
+    canVote = (age >= 18) ? "Woohoo! You are old enough":"Sorry, you are too young";
+    document.getElementById("vote").innerHTML = canVote + " to vote!";
+}
+
 function Vehicle(Make, Model, Year, Color) {
     this.Vehicle_Make = Make;
     this.Vehicle_Model = Model;
