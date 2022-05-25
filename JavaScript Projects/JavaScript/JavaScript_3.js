9f28be98358d4e064d40d5e47c7169168dd2172b
Samantha <samanthax0326@gmail.com>
2022-05-25 09:06:33 -0700

    food type function commit

--- JavaScript Projects/JavaScript/JavaScript_3.js
+++ JavaScript Projects/JavaScript/JavaScript_3.js
@@ -1,4 +1,4 @@
-function displayType(food) {
+function displayType(food) { //my food function
     var foodType = food.getAttribute("data-food_type");
     alert("A " + food.innerHTML + " is a " + foodType + "!");
 }
